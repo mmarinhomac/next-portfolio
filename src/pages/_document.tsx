75d99bd1160a8a78866e2366eaf300d9865a3863
Matheus Marinho <matheus.marinho@manipulae.com.br>
2024-02-04 04:52:56 -0300

    feat: personal analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,6 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+declare let dataLayer: any[];
 
 export default class MyDocument extends Document {
   render() {
@@ -6,18 +8,25 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400&display=swap" rel="stylesheet" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400&display=swap"
+            rel="stylesheet"
+          />
 
           <link rel="shortcut icon" href="/favicon.png" type="image/png" />
 
           <script async src="https://www.googletagmanager.com/gtag/js?id=G-JP146EBRGV"></script>
-          <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){ dataLayer.push(arguments); }
-            gtag('js', new Date());
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
 
-            gtag('config', 'G-JP146EBRGV');
-          </script>
+                gtag('config', 'G-JP146EBRGV');
+              `,
+            }}
+          />
         </Head>
         <body>
           <Main />
